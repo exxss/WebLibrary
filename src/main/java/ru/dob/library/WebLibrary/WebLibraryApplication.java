ede1efee4ac4e7e463a9090f1a16be1c3902f309
Nikita Dobysh <nikita.ya.exs@gmail.com>
2022-10-05 12:10:13 +0300

    add weblibrary

---
+++ src/main/java/ru/dob/library/WebLibrary/WebLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.dob.library.WebLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dob/library/WebLibrary/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dob.library.WebLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
